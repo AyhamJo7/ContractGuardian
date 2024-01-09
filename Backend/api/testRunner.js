1480006c8556b3bd4473d779e9411ea5e8c4ee86
AyhamJo7 <ayhamjumran7@gmail.com>
2024-01-09 17:57:53 +0100

    Project Structure

--- Backend/api/testRunner.js
+++
@@ -1,9 +0,0 @@
-const runPythonScript = require('./pythonScriptRunner');
-
-runPythonScript('C:\\Users\\ayham\\Desktop\\Erweiterter_Dummy_GmbH_Gesellschaftervertrag.pdf', (error, results) => {
-    if (error) {
-        console.error("Error:", error);
-    } else {
-        console.log("Results:", results);
-    }
-});
