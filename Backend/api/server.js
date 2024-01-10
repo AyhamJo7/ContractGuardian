c526722a47301b2672c9f03b69d2f3020032d2f1
AyhamJo7 <ayhamjumran7@gmail.com>
2024-01-10 02:25:54 +0100

    app.listen(PORT

--- Backend/api/server.js
+++ Backend/api/server.js
@@ -1,23 +1,34 @@
 const express = require("express");
-const app = express();
 const cookieParser = require("cookie-parser");
 const analyzeRoute = require("./routes");
 
+require('dotenv').config();
 
+const app = express();
+
+// Body-Parser Middleware, um JSON-Daten zu verarbeiten
 app.use(express.json({ limit: "10mb" }));
 app.use(cookieParser());
 
-// Set up CORS headers
+// CORS-Headers einrichten
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+
+  const allowedOrigins = ['http://localhost:3000', 'https://your-heroku-app-name.herokuapp.com']; // Zugriff erlauben
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+      res.setHeader('Access-Control-Allow-Origin', origin);
+  }
   res.setHeader("Access-Control-Allow-Credentials", "true");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE"); // Erlaubte Methoden
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization"); // Erlaubte Header
   next();
 });
 
 app.use("/api/v1", analyzeRoute);
 
-app.listen(4000, () => {
-  console.log(`Server is running on port 4000`);
+// Use environment variable for port or default to 4000
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
