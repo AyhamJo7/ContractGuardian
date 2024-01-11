2f35a21b21fa684ff6f987e55a43acbbccde482a
AyhamJo7 <ayhamjumran7@gmail.com>
2024-01-11 12:47:04 +0100

    Fix issues with DarkModeToggler

--- Frontend/src/components/DarkModeToggle.js
+++ Frontend/src/components/DarkModeToggle.js
@@ -3,7 +3,7 @@ import './DarkModeToggle.css'
 
 const DarkModeToggle = ({ isDarkMode, onToggle }) => {
   return (
-    <div className="container">
+    <div className="div relative flex justify-left overflow-hidden px-4 py-2 lg:flex-row lg:items-center">
       <input
         id="checkbox"
         type="checkbox"
